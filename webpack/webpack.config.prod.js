c9997efe8493bc54c73c33da1e31456438187ea3
kenkz447 <kenkz447@gmail.com>
2019-09-02 21:57:43 +0700

    change API entry

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -6,8 +6,8 @@ module.exports = getBuildConfig({
     definitions: {
         VERSION_HASH: '$Id$',
         SUB_ENV: 'production',
-        FILE_HOST: 'http://landing-admin.mfurniture.vn',
-        API_ENTRY: 'http://landing-admin.mfurniture.vn',
+        FILE_HOST: 'http://landing.mfurniture.vn',
+        API_ENTRY: 'http://landing.mfurniture.vn',
         SENTRY_ID: 'https://e51942a013484d4dbfab39db75aa25f5@sentry.io/1549017',
     },
     sourceMap: true,
