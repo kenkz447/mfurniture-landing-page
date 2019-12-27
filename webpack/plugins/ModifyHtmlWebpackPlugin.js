a48d09a9e3a8c0897c03e70afe62296fbbb9d4c1
kenkz447 <kenkz447@gmail.com>
2019-12-27 13:25:21 +0700

    update site og

--- webpack/plugins/ModifyHtmlWebpackPlugin.js
+++ webpack/plugins/ModifyHtmlWebpackPlugin.js
@@ -15,6 +15,11 @@ class ModifyHtmlWebpackPlugin {
                     html: html
                         .replace('<html>', `<html lang="${this.lang}">`)
                         .replace('<title>', `<meta name="p:domain_verify" content="ac31560091f308d48fb21c3bb8e8bc2c"/><title>`)
+                        .replace('<title>', `<meta property="og:type" content="website" /><title>`)
+                        .replace('<title>', `<meta property="og:image" content="https://landing.mfurniture.vn/uploads/7c79ba70035243b2a8c83f732aa88fec.jpg" /><title>`)
+                        .replace('<title>', `<meta property="og:image:alt" content="M Collection" /><title>`)
+                        .replace('<title>', `<meta property="og:title" content="[M]Furniture" /><title>`)
+                        .replace('<title>', `<meta property="og:description" content="https://mfurniture.vn" /><title>`)
                         .replace('<body>', `<body>\n<noscript>${this.noscriptMessage}</noscript>`)
                 });
             });
